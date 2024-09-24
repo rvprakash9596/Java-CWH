61503842abc42afc02753286413060968df011cb
Ravi Prakash <126048086+rvprakash9596@users.noreply.github.com>
2024-09-24 12:41:11 +0530

    Even and Odd Number

---
+++ EvenOdd.java
@@ -0,0 +1,13 @@
+class EvenOdd{
+public static void main(String args[])
+{
+	int n1=45;
+	int n2=42;
+	if(n1%2==0){
+		System.out.println("Number is even");
+	}
+	else{
+		System.out.println("Number is odd");
+	}
+}
+}
